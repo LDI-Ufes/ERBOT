94ab5f26f2c457ec26878a9dfdebfaac744814ae
Paulo <pauloserpa.r@gmail.com>
2020-03-18 15:23:25 -0300

    7. Tabelas v1

---
+++ components/tabela-nao-socio.php
@@ -0,0 +1,34 @@
+<table>
+    <tr>
+        <th>Inscrição</th>
+        <th>Até 30/04</th> 
+        <th>Até 01/05</th>
+    </tr>
+    <tr>
+        <td>Profissional</td>
+        <td>R$ 380,00</td>
+        <td>R$ 395,00</td>
+    </tr>
+    <tr>
+        <td>Pós-graduando</td>
+        <td>R$ 270,00</td>
+        <td>R$ 285,00</td>
+    </tr>
+    <tr>
+        <td>Graduando</td>
+        <td>R$ 80,00</td>
+        <td>R$ 95,00</td>
+    </tr>
+    <tr>
+        <td>Professor da Educação Básica</td>
+        <td colspan="2">R$ 40,00</td>
+    </tr>
+    <tr>
+        <td>Estudante de Curso Técnico</td>
+        <td colspan="2">R$ 30,00</td>
+    </tr>
+    <tr>
+        <td>Estudante da Educação Básica</td>
+        <td colspan="2">R$ 20,00</td>
+    </tr>
+    </table>
---
+++ components/tabela-socio.php
@@ -0,0 +1,34 @@
+<table>
+    <tr>
+        <th>Inscrição</th>
+        <th>Até 30/04</th> 
+        <th>Até 01/05</th>
+    </tr>
+    <tr>
+        <td class="descricao">Profissional</td>
+        <td class="preco">R$ 90,00</td>
+        <td class="preco">R$ 105,00</td>
+    </tr>
+    <tr>
+        <td class="descricao">Pós-graduando</td>
+        <td class="preco">R$ 50,00</td>
+        <td class="preco">R$ 65,00</td>
+    </tr>
+    <tr>
+        <td class="descricao">Graduando</td>
+        <td class="preco">R$ 40,00</td>
+        <td class="preco">R$ 55,00</td>
+    </tr>
+    <tr>
+        <td class="descricao">Professor da Educação Básica</td>
+        <td class="preco" colspan="2">R$ 40,00</td>
+    </tr>
+    <tr>
+        <td class="descricao">Estudante de Curso Técnico</td>
+        <td class="preco" colspan="2">R$ 30,00</td>
+    </tr>
+    <tr>
+        <td class="descricao">Estudante da Educação Básica</td>
+        <td class="preco" colspan="2">R$ 20,00</td>
+    </tr>
+    </table>
