cfbef19fd957c4315c4b7fb0b444e7109dd8d2ea
Paulo <pauloserpa.r@gmail.com>
2020-04-13 16:30:54 -0300

    Footer v1

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,39 @@
+<footer>
+    <div class="container">
+        
+    <img class="logo-footer" src="img/footer/logo-footer.png" alt="">
+
+        <div>
+            <h2>Sociedade Brasileira de Botânica - Regional MG, BA, ES</h2>
+            <p>Sediado na Universidade Federal do Espírito Santo (UFES)</p>
+            <h2>Fale Conosco</h2>
+            <p><strong>Email: </strong>40erbot@gmail.com</p>
+            <p><strong>Telefone: </strong>27 3333-3333</p>
+        </div>
+        <div>
+            <h2>Promoção</h2>
+            <img src="img/footer/logo-SBB.png" alt="">
+        </div>
+
+        <div>
+            <h2>Realização</h2>
+            <img src="img/footer/logo-UFES.png" alt="">
+        </div>
+
+        <div>
+            <h2>Apoio</h2>
+            <img src="img/footer/logo-CAPES.png" alt="">
+            <img src="img/footer/logo-CAPES.png" alt="">
+            <img src="img/footer/logo-CAPES.png" alt="">
+            <img src="img/footer/logo-CAPES.png" alt="">
+       </div>
+    </div>
+    <div class="copyright">
+        <p>2020 | Desenvolvido por LDI</p>
+        <img src="img/logo-ldi.svg" alt="">
+    </div>
+</footer>
+
 <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="js/focus-visible.min.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
